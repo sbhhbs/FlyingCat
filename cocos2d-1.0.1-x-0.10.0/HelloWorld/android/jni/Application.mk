bf5bfdc4c4957ebf3cadead764a40d375d8f5579
sbhhbs <sbhhbscbfv@gmail.com>
2011-12-15 15:46:00 +0800

    What! Signed-off-by: sbhhbs <sbhhbscbfv@gmail.com>

---
+++ cocos2d-1.0.1-x-0.10.0/HelloWorld/android/jni/Application.mk
@@ -0,0 +1,6 @@
+# it is needed for ndk-r5
+APP_STL := stlport_static
+
+APP_ABI := armeabi armeabi-v7a
+
+APP_MODULES := cocos2d cocosdenshion game_logic helloworld
---
+++ cocos2d-1.0.1-x-0.10.0/tests/test.android/jni/Application.mk
@@ -0,0 +1,5 @@
+# it is needed for ndk-r5
+APP_STL := stlport_static
+APP_ABI := armeabi
+
+APP_MODULES := cocos2d cocosdenshion chipmunk box2d game_logic tests
